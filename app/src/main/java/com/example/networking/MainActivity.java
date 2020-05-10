1fdc214960244f8f079c113f60f413b511638330
Viktor Thiberg <a19vikth@student.his.se>
2020-05-10 14:22:33 +0200

    Added AsyncTask code to get the jsonfile and to connect the data to textview and listview

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,7 +2,29 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +32,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String json) {
+            try {
+                final String[] name = new String[3];
+                final String[] location = new String[3];
+                final String[] size = new String[3];
+                JSONArray json1 = new JSONArray(json);
+                for (int i = 0; i < 3; i++) {
+                    JSONObject JsonObject = json1.getJSONObject(i);
+
+                    location[i] = JsonObject.getString("location");
+                    size[i] = JsonObject.getString("size");
+                    name[i] = JsonObject.getString("name");
+
+                }
+                ArrayList<String> ListData=new ArrayList<>(Arrays.asList(name));
+                ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,R.layout.list_item_textview,R.id.list_item_textview_xml,ListData);
+                ListView my_listview=findViewById(R.id.list_item_textview);
+                my_listview.setAdapter(adapter);
+                my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getApplicationContext(),name[position]+" is located in "+location[position]+" and is "+size[position]+"meter tall.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }catch (JSONException e){
+                Log.e("brom","E:"+e.getMessage());
+            }
+        }
     }
 }
